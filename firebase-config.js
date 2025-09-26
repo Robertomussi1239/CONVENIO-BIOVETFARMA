db2206ae84ab3cd1e7b6eacd04263dc0ad7f2602
Robertomussi1239 <roberto.mussi@outlook.com>
2025-09-26 18:34:53 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDs3kSM_XlggDisVTIuJI6CMXEi6aQ_i5Y",
   authDomain: "convenio-biovetfarma.firebaseapp.com",
@@ -7,4 +8,5 @@ const firebaseConfig = {
   appId: "1:125449715016:web:f8c830487acca13667477d",
   measurementId: "G-LCNTPNGMJX"
 };
+
 firebase.initializeApp(firebaseConfig);
